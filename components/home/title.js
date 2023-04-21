2ea859e6b4404a089c2e298e180788f1899529f8
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-20 22:09:51 -0700

    found easy way to import google fonts -- functional

--- components/home/title.js
+++ components/home/title.js
@@ -1,14 +1,31 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
-const Title = () => (
-    <View style={styles.container}>
-        <Text style={styles.title}>GetAHobby</Text>
-    </View>
-);
+import { Text, View, StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, ComicNeue_300Light } from '@expo-google-fonts/comic-neue';
+
+const Title = () => {
+    let [fontsLoaded] = useFonts({ComicNeue_300Light});
+    let fontSize = 24;
+    let paddingVertical = 6;
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    } else {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>GetAHobby</Text>
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     container: {},
-    title: {}
+    title: {
+        fontFamily: "ComicNeue_300Light",
+        fontSize: 50
+    }
 });
 
 export default Title;
