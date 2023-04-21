8e7e104a31b89e6ee87c69d345127910a454589a
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-20 19:14:46 -0700

    added home/title component frame

---
+++ components/home/title.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const Title = () => (
+    <View style={styles.container}>
+        <Text style={styles.title}>GetAHobby</Text>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {},
+    title: {}
+});
+
+export default Title;
