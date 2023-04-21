c918d253bc57c75e6a71108fdde9c22d05736cbc
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-20 22:10:57 -0700

    removed excess code

--- components/home/title.js
+++ components/home/title.js
@@ -1,13 +1,9 @@
-import React, { useState, useEffect } from 'react';
-
 import { Text, View, StyleSheet } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, ComicNeue_300Light } from '@expo-google-fonts/comic-neue';
 
 const Title = () => {
     let [fontsLoaded] = useFonts({ComicNeue_300Light});
-    let fontSize = 24;
-    let paddingVertical = 6;
 
     if (!fontsLoaded) {
         return <AppLoading />
