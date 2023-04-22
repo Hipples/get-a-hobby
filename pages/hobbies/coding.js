c28b87a927f817a6171ae437468468dc4f085848
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-21 20:37:00 -0700

    create coding hobby template

---
+++ pages/hobbies/coding.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const Coding = () => (
+    <View style={styles.container}>
+        <Text>Coding</Text>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {}
+});
+
+export default Coding;
