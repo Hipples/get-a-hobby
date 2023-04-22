fa5e146fd2d88b3259befea1a9063013332d2963
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-22 16:16:21 -0700

    research notes for coding hobby

---
+++ constants/resources.js
@@ -0,0 +1,10 @@
+//general info on coding hobby
+const happyhobbyists = 'https://www.happyhobbyists.com/programming/programming-as-a-hobby/';
+//tips
+const hubspot = 'https://blog.hubspot.com/website/how-to-start-coding';
+//games
+const makeuseof = 'https://www.makeuseof.com/tag/best-programming-games/';
+//top 10 programming languages
+const sam_solutions='https://www.sam-solutions.com/blog/top-10-programming-languages-and-their-use-cases/';
+//large repo of language docs
+const devdocs='https://devdocs.io/python~3.11/';
