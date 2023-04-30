492572615644cdb93003845bd985e5f9035394cc
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-29 20:55:39 -0700

    comments added

--- components/common/difficulty-rating.js
+++ components/common/difficulty-rating.js
@@ -1,12 +1,15 @@
+// more info on rating props can be found at the link below:
+// https://github.com/Monte9/react-native-ratings
+
 import { View, StyleSheet } from 'react-native';
 import { Rating } from 'react-native-ratings';
 
 const DifficultyRating = ({ rating }) => (
     <View style={styles.container}>
         <Rating 
-            readonly={true}
-            ratingCount={5}
-            startingValue={rating}
+            readonly={true}  // user cannot modify star display
+            ratingCount={5}  // easy, novice, intermediate, advanced, expert
+            startingValue={rating}  // sets number of filled stars to reflect rating
             />
     </View>
 );
