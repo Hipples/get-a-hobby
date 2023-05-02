71881530d9591d93c5818e66b64c66f09f8f7645
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-05-02 13:39:26 -0700

    playing with tip styles

--- pages/hobbies/coding.js
+++ pages/hobbies/coding.js
@@ -60,22 +60,27 @@ const HobbyHealthAndSafety = () => (
     </View>
 );
 
-const tips = [
+const codingTips = [
     {
-        index: 1,
-        tip: "Build projects that excite you: Coding can be a lot of fun when you're working on something that interests you. So, choose projects that inspire you, whether it's creating a game, building a website, or automating a task you find repetitive."
+        tip: 1,
+        title: "Build Projects That Excite You!",
+        explanation: "Coding can be a lot of fun when you're working on something that interests you. So, choose projects that inspire you, whether it's creating a game, building a website, or automating a task you find repetitive."
     },{
-        index: 2,
-        tip: "Don't be afraid to ask for help: The coding community is incredibly supportive, so don't hesitate to reach out to others for help and guidance. There are plenty of online forums and communities where you can connect with other coders and learn from their experiences."
+        tip: 2,
+        title: "Dont Be Afraid to Ask for Help!",
+        explanation: "The coding community is incredibly supportive, so don't hesitate to reach out to others for help and guidance. There are plenty of online forums and communities where you can connect with other coders and learn from their experiences."
     },{
-        index: 3,
-        tip: "Experiment with different programming languages: Don't be afraid to try different programming languages and tools to find the ones that work best for you. Each language has its own strengths and weaknesses, and you may find that you enjoy certain languages more than others.",
+        tip: 3,
+        title: "Experiment with Different Programming Languages!",
+        explanation: "Don't be afraid to try different programming languages and tools to find the ones that work best for you. Each language has its own strengths and weaknesses, and you may find that you enjoy certain languages more than others.",
     },{
-        index: 4,
-        tip: "Experiment and make mistakes: Making mistakes is a natural part of the learning process. So, don't be afraid to experiment and try new things, even if it means making errors along the way. Learning from your mistakes is an important part of becoming a better coder.",
+        tip: 4,
+        title: "Experiment and Make Mistakes!",
+        explanation: "Making mistakes is a natural part of the learning process. So, don't be afraid to experiment and try new things, even if it means making errors along the way. Learning from your mistakes is an important part of becoming a better coder.",
     },{
-        index: 5,
-        tip: "Keep learning: The world of coding is constantly evolving, with new technologies and techniques emerging all the time. So, make sure to stay up-to-date with the latest trends and developments by reading blogs, attending webinars, and taking online courses. Continuous learning is essential to stay ahead of the game and to keep enjoying coding as a hobby."
+        tip: 5,
+        title: "Keep Learning!",
+        explanation: "The world of coding is constantly evolving, with new technologies and techniques emerging all the time. So, make sure to stay up-to-date with the latest trends and developments by reading blogs, attending webinars, and taking online courses. Continuous learning is essential to stay ahead of the game and to keep enjoying coding as a hobby."
     }]
 
 const item_width = Dimensions.get('window').width
@@ -89,10 +94,12 @@ const HobbyTips = () => {
             snapToInterval={item_width}
             bounces={false}
             showsHorizontalScrollIndicator={false}>
-        {tips.map((the) => {
+        {codingTips.map((each) => {
             return (
-                <View key={the.index} style={styles.tipContainer}>
-                    <Text style={styles.descriptionText}>{the.tip}</Text>
+                <View key={each.tip} style={styles.tipContainer}>
+                    <Text style={styles.tipHeader}>Tip {each.tip}</Text>
+                    <Text style={styles.tipTitle}>{each.title}</Text>
+                    <Text style={styles.tipText}>{each.explanation}</Text>
                 </View>
             );
         })}
@@ -133,7 +140,7 @@ const styles = StyleSheet.create({
         color: '#EFEFEF'
     },
     imageContainer: {
-        height: 250,
+        height: 300,
         marginHorizontal: 10,
         marginBottom: 10,
         justifyContent: 'center',
@@ -142,8 +149,8 @@ const styles = StyleSheet.create({
         backgroundColor: "#666"
     },
     image: {
-        height: 250,
-        width: 250,
+        height: 350,
+        width: 350,
     },
     descriptionContainer: {
         height: 700,
@@ -162,12 +169,39 @@ const styles = StyleSheet.create({
         color: "#EFEFEF"
     },
     tipContainer: {
-        height: 300,
+        justifyContent: 'space-around',
+        height: 500,
         width: item_width-20,
-        borderWidth: 2,
+        backgroundColor: '#DDD',
+        borderWidth: 3,
         marginHorizontal: 10,
         marginBottom: 10,
-        padding: 10
+        padding: 10,
+        elevation: 2,
+        shadowColor: '#000',
+
+    },
+    tipHeader: {
+        textAlign: 'center',
+        fontSize: 30,
+        color: '#335',
+        fontWeight: 'bold',
+
+    },
+    tipTitle: {
+        textAlign: 'center',
+        fontSize: 25,
+        color: '#335',
+        fontStyle: 'italic',
+        paddingHorizontal: 10,
+
+    },
+    tipText: {
+        fontSize: 20,
+        paddingHorizontal: 5,
+        color: '#335',
+        textAlign: 'justify'
+
     }
 });
 
