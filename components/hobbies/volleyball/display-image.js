eee0978d70639a6fc730a0671ed24a85d861eae5
Mary Oh <marychristineoh@gmail.com>
2023-05-12 07:46:03 +0000

    added volleyball page

---
+++ components/hobbies/volleyball/display-image.js
@@ -0,0 +1,30 @@
+import { View, Image, StyleSheet } from 'react-native';
+
+const codingImage = require('../../../assets/volleyball.png');
+
+const DisplayImage = () => (
+    <View style={styles.imageContainer}>
+        <Image
+            style={styles.image} 
+            source={codingImage}
+            resizeMode='contain' />
+    </View>
+);
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        height: 300,
+        marginHorizontal: 10,
+        marginBottom: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        backgroundColor: "#666"
+    },
+    image: {
+        height: 350,
+        width: 350
+    }
+});
+
+export default DisplayImage;
---
+++ constants/volleyball-tips.js
@@ -0,0 +1,24 @@
+const volleyballTips = [
+    {
+        index: 1,
+        title: "If you can't serve, you can't play. ",
+        explanation: "Each official match must begin with a serve from one team on defense which is launched into the court of the opposing team who's receiving serve by a player in their service area behind the end line with the intent on starting the rally and possibly scoring a point"
+    },{
+        index: 2,
+        title: "Passing is the most important skill.",
+        explanation: "There are three volleyball forearm pass cues you should use to improve your passing skills, keep your arms together, feet grounded and hips behind the ball."
+    },{
+        index: 3,
+        title: "Learning to hit is a big deal because you score points for the team.",
+        explanation: "The volleyball spike approach for hitting is also called the spike approach and is done by a spiker who takes 3 or 4 steps to a ball in the air before hitting it. For 3 steps approach, the directional step is the first of three which you'd use to propel yourself closer to where you want to contact the ball.",
+    },{
+        index: 4,
+        title: "Blocking at the net is the first line of defense.",
+        explanation: "Blocking is the first line of defense. To block well you are required to have good footwork, have great body control while moving laterally and vertically, read your hitter to anticipate where and how she plans to attack, and penetrate your hands over the net to block the ball by deflecting it back into your opponents court",
+    },{
+        index: 5,
+        title: "Digging the ball sometimes means you have to hit the floor.",
+        explanation: "To dig a volleyball that's more than 2-3 steps away from you and low to the floor, you need to know how to play balls up on the move, then complete your digging action and fall to the ground in a safe way that you get back up and keep playing without getting hurt."
+    }];
+
+export default volleyballTips;
