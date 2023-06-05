77110d96c193262a9cd549367e67603ed389013a
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-06-04 20:18:38 -0700

    create user context, install async storage

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+import UserProvider from './contexts/user-context';
 import Welcome from './pages/welcome';
 import RootNav from './navigation/root-nav';
 
@@ -10,14 +11,16 @@ const WelcomeStack = createNativeStackNavigator();
 
 // navigation wrapper for entire app 
 const App = () => (
-    <NavigationContainer>
-        <WelcomeStack.Navigator 
-            initialRouteName="Welcome"
-            screenOptions={{ headerShown: false }} >
-            <WelcomeStack.Screen name="Welcome" component={Welcome} />
-            <WelcomeStack.Screen name="Root" component={RootNav} />
-        </WelcomeStack.Navigator>
-    </NavigationContainer>
+    <UserProvider>
+        <NavigationContainer>
+            <WelcomeStack.Navigator 
+                initialRouteName="Welcome"
+                screenOptions={{ headerShown: false }} >
+                <WelcomeStack.Screen name="Welcome" component={Welcome} />
+                <WelcomeStack.Screen name="Root" component={RootNav} />
+            </WelcomeStack.Navigator>
+        </NavigationContainer>
+    </UserProvider>
 );
 
 export default App;
---
+++ contexts/user-context.js
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from "react";
+
+export const UserContext = createContext();
+export const useUserContext = () => useContext(UserContext);
+
+const UserProvider = ({ children }) => {
+    [ user, setUser ] = useState("");
+
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+            { children }
+        </UserContext.Provider>
+    );
+}
+
+export default UserProvider;
