eff14f465d45ff9ef7d75a5e2aa833689efd722e
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-06-05 16:27:35 -0700

    refactoring

--- components/settings/create-user.js
+++ components/settings/create-user.js
@@ -1,28 +1,19 @@
-import { useEffect, useState } from 'react';
-import { Dimensions, View, Button, Text, StyleSheet, TextInput, Alert } from 'react-native';
+import { useState } from 'react';
+import { View, Button, Text, TextInput } from 'react-native';
 import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 import { useUserContext } from '../../contexts/user-context';
-
-const screenWidth = Dimensions.get('window').width
+import { styles } from './settings-styles'
 
 const CreateUser = ({ navigation }) => {
     const [ value, setValue ] = useState('');
-    const { user, setUser } = useUserContext();
-    const { setItem, getItem } = useAsyncStorage('@user');
+    const { setUser } = useUserContext();
+    const { setItem } = useAsyncStorage('@user');
 
     const storeUser = async (newValue) => {
         try { await setItem(newValue); setUser(newValue); } 
         catch (error) { console.error(error)}
     }
-    const getUser = async() => {
-        try {
-            const name = await getItem();
-            setUser(name);
-            console.log(`New user stored: ${user}`)
-        } catch (error) { console.error(error) }
-    }
-
     const handleContinue = () => {
         if (value.length === 0) {
             alert("Please enter at least one character!")
@@ -34,61 +25,35 @@ const CreateUser = ({ navigation }) => {
         navigation.goBack();
     }
  
-    // useEffect(() => { getUser() }, []);
-
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={styles.loginPage}>
         <View style={styles.loginContainer}>
+          
           <Text 
             label="Name" 
-            style={styles.promptText}>
+            style={styles.loginText}>
               Enter your name to continue
           </Text>
+
           <TextInput 
             value={value} 
             onChangeText={(newValue) => setValue(newValue)}
             style={styles.userInput} />
+          
           <View style={styles.buttonsContainer}>
-          <Button title="Continue" style={styles.continue} onPress={() => handleContinue()} />
-          <Button title="Go Back" style={styles.goBack} onPress={() => handleGoBack()}/>
+            <Button 
+              title="Continue" 
+              style={styles.continue} 
+              onPress={() => handleContinue()} />
+            <Button 
+              title="Go Back" 
+              style={styles.goBack} 
+              onPress={() => handleGoBack()}/>
           </View>
+
         </View>
       </View>
     );
 }
-
-const styles = StyleSheet.create({
-    loginContainer: {
-      flexBasis: 300,
-      width: screenWidth-20,
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      backgroundColor: 'white',
-      padding: 20, 
-      borderRadius: 20
-    },
-    promptText: {
-      fontSize: 20,
-      color: 'black',
-      marginBottom: 10
-    },
-    userInput: {
-      flexBasis: 100,
-      fontSize: 50,
-      padding: 10,
-      width: screenWidth-50,
-      backgroundColor: 'aliceblue',
-      borderWidth: 1
-    },
-    buttonsContainer: {
-      flexDirection: 'row', 
-      marginTop: 10,
-      columnGap: 10,
-      width: screenWidth-50,
-      justifyContent: 'flex-end',
-    },
-    continue: {},
-    goBack: {}
-  });
 
 export default CreateUser;
--- components/settings/settings-styles.js
+++ components/settings/settings-styles.js
@@ -1,4 +1,6 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+
+const screenWidth = Dimensions.get('window').width
 
 export const styles = StyleSheet.create({
     deleteUserButtonContainer: {
@@ -13,6 +15,38 @@ export const styles = StyleSheet.create({
         width: 200
     },
     deleteUserButtonLabel: {
-
-    }
-})
+        fontSize: 20
+    },
+    loginContainer: {
+        flexBasis: 300,
+        width: screenWidth-20,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        padding: 20, 
+        borderRadius: 20
+      },
+      loginText: {
+        fontSize: 20,
+        color: 'black',
+        marginBottom: 10
+      },
+      userInput: {
+        flexBasis: 100,
+        fontSize: 50,
+        padding: 10,
+        width: screenWidth-50,
+        backgroundColor: 'aliceblue',
+        borderWidth: 1
+      },
+      buttonsContainer: {
+        flexDirection: 'row', 
+        marginTop: 10,
+        columnGap: 10,
+        width: screenWidth-50,
+        justifyContent: 'flex-end',
+      },
+      continueButton: {},
+      goBackButton: {},
+      loginPage: {flex: 1, justifyContent: 'center', alignItems: 'center'}
+});
