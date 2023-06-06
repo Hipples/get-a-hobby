331ffc5396c7b37a5dcb8931783d722babf25bf0
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-06-06 16:55:14 -0700

    basically done

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,22 +1,26 @@
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+import { SettingsBackground } from '../components/settings/settings-styles';
 
 const AboutUs = () => (
-    <View>
-        <Text style={{padding: 10}}>
-            Welcome to GetAHobby!
+    <SettingsBackground>
+        <Text style={{paddingHorizontal: 10, lineHeight: 20}}>
+            Thank you for checking out GetAHobby!
             {'\n\n'}
-            We are a team of MSCS students from City University of Seattle. This app is a term project for CS 624: Full-Stack Development (Mobile Apps) in Spring of 2023. GetAHobby has been designed to demonstrate the development process of a React Native cross-platform mobile application. 
+            We are a team of MSCS students from City University of Seattle. This app is a term project for CS 624: Full-Stack Development (Mobile Apps) in Spring of 2023.
             {'\n\n'}
-            The following concepts are demonstrated in GetAHobby:{'\n'}
-            - Reuseable Components{'\n'}
-            - StyleSheet Designs{'\n'}
-            - React Navigation{'\n'}
-            - React Hooks{'\n'}
-            - React Context{'\n'}
-            - Fetch API{'\n'}
-            - Async Storage{'\n'}
+            GetAHobby has been designed to demonstrate the development process of a React Native cross-platform mobile application. 
+            {'\n\n'}
+            GetAHobby demonstrates the following concepts:{'\n'}
+            {'\t'}- Reuseable UI Components{'\n'}
+            {'\t'}- React Native StyleSheet Designs{'\n'}
+            {'\t'}- React Hooks (useState, useEffect, useContext, etc.){'\n'}
+            {'\t'}- React Navigation (stacks, bottom-tabs, drawer){'\n'}
+            {'\t'}- React Context{'\n'}
+            {'\t'}- Fetch API (using Bored API){'\n'}
+            {'\t'}- Async Storage (storing username){'\n'}
         </Text>
-    </View>
+    </SettingsBackground>
 );
 
 export default AboutUs;
